d9ae5eb9a3aaff9100adef52c4b853317e26e9c9
Germán <GassetGerman@gmail.com>
2022-09-08 10:48:09 +0200

    added basic form controls Added trainFromImagesToolStrinpMenuItem_Click(), FilterFiles() and FolderToName() at Form1.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AbstractVideoGenerator
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trainFromImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.trainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "Menu";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToFolderToolStripMenuItem});
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveToFolderToolStripMenuItem
+            // 
+            this.saveToFolderToolStripMenuItem.Name = "saveToFolderToolStripMenuItem";
+            this.saveToFolderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToFolderToolStripMenuItem.Text = "Save to folder";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFromFileToolStripMenuItem});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            this.loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            this.loadFromFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.loadFromFileToolStripMenuItem.Text = "Load from file";
+            // 
+            // trainToolStripMenuItem
+            // 
+            this.trainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trainFromImagesToolStripMenuItem});
+            this.trainToolStripMenuItem.Name = "trainToolStripMenuItem";
+            this.trainToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.trainToolStripMenuItem.Text = "Train";
+            // 
+            // trainFromImagesToolStripMenuItem
+            // 
+            this.trainFromImagesToolStripMenuItem.Name = "trainFromImagesToolStripMenuItem";
+            this.trainFromImagesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.trainFromImagesToolStripMenuItem.Text = "Train from images";
+            this.trainFromImagesToolStripMenuItem.Click += new System.EventHandler(this.trainFromImagesToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Video Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainFromImagesToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,66 @@ using System.Windows.Forms;
 
 namespace AbstractVideoGenerator
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        static string[] supportedExtensions = new string[] { "JPG", "JPEG", "PNG" };
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void trainFromImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()
+            {
+                Description = "You must select a folder with folders that contains images"
+            };
+
+            folderBrowserDialog.ShowDialog();
+
+            List<string> directories = new List<string>(Directory.GetDirectories(folderBrowserDialog.SelectedPath));
+
+            List<string[]> imagesPaths = new List<string[]>();
+            foreach (var imageDirectory in directories)
+            {
+                imagesPaths.Add(FilterFiles(Directory.GetFiles(imageDirectory)));
+            }
+
+            List<int> emptyFoldersIndexes = new List<int>();
+            for (int i = 0; i < directories.Count; i++)
+                if (imagesPaths[i].Length == 0)
+                    emptyFoldersIndexes.Add(i);
+
+            for (int i = emptyFoldersIndexes.Count - 1; i >= 0; i--)
+            {
+                directories.RemoveAt(emptyFoldersIndexes[i]);
+                imagesPaths.RemoveAt(emptyFoldersIndexes[i]);
+            }
+        }
+
+        public string[] FilterFiles(string[] filePaths)
+        {
+            var output = new List<string>();
+            foreach (var filePath in filePaths)
+            {
+                bool containsSupportedExtension = false;
+                foreach (var supportedExtension in supportedExtensions)
+                    containsSupportedExtension = filePath.Contains(supportedExtension) || containsSupportedExtension;
+
+                if (containsSupportedExtension)
+                    output.Add(filePath);
+            }
+            return output.ToArray();
+        }
+
+        public static string FolderToName(string folderPath)
+        {
+            if (folderPath.EndsWith(@"\"))
+                folderPath = folderPath.Remove(folderPath.LastIndexOf(@"\"));
+
+            folderPath = folderPath.Remove(0, folderPath.LastIndexOf(@"\"));
+            return folderPath;
         }
     }
 }
