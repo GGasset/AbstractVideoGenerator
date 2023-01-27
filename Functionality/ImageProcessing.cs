f99c43f1abd507a5826023b3ffbedd26831da0fd
Germán Gasset <gassetgerman@gmail.com>
2023-01-27 14:07:58 +0100

    added support for transforming and using color to greyScaleImages

--- Functionality/ImageProcessing.cs
+++ Functionality/ImageProcessing.cs
@@ -45,132 +45,160 @@ namespace Functionality
             return output;
         }
 
-        public static double[] BitmapToDoubleArray(Bitmap bitmap, double dataDividend = 255)
+        public static double[] BitmapToDoubleArray(Bitmap bitmap, bool isGreyScale = false, double dataDividend = 255)
         {
             int imageSize = bitmap.Height * bitmap.Width;
             int bitmapData = imageSize * 3;
 
-            double[] output = new double[bitmapData];
+            double[] output = new double[isGreyScale? imageSize : bitmapData];
             int i = 0;
-            for (int x = 0; x < bitmap.Width; x++)
-            {
-                for (int y = 0; y < bitmap.Height; y++)
+            if (!isGreyScale)
+                for (int x = 0; x < bitmap.Width; x++)
                 {
-                    Color cPixel = bitmap.GetPixel(x, y);
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        Color cPixel = bitmap.GetPixel(x, y);
 
-                    output[i] = cPixel.R / dataDividend;
-                    i++;
-                    output[i] = cPixel.G / dataDividend;
-                    i++;
-                    output[i] = cPixel.B / dataDividend;
-                    i++;
+                        output[i] = cPixel.R / dataDividend;
+                        i++;
+                        output[i] = cPixel.G / dataDividend;
+                        i++;
+                        output[i] = cPixel.B / dataDividend;
+                        i++;
+                    }
                 }
-            }
+            else
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        Color cPixel = bitmap.GetPixel(x, y);
+
+                        int color = cPixel.R + cPixel.G + cPixel.B;
+                        color /= 3;
+
+                        output[i] = color / dataDividend;
+                    }
+                }
+
             return output;
         }
 
-        public static Bitmap DoubleArrayToBitmap(double[] imageData, int bitmapWidth, int bitmapHeight, double dataMultiplier = 255)
+        public static Bitmap DoubleArrayToBitmap(double[] imageData, int bitmapWidth, int bitmapHeight, bool isGreyScale = false, double dataMultiplier = 255)
         {
             int counter = 0;
             Bitmap output = new Bitmap(bitmapWidth, bitmapHeight);
-            for (int x = 0; x < bitmapWidth; x++)
-            {
-                for (int y = 0; y < bitmapHeight; y++)
+            if (!isGreyScale)
+                for (int x = 0; x < bitmapWidth; x++)
                 {
-                    byte R, G, B;
+                    for (int y = 0; y < bitmapHeight; y++)
+                    {
+                        byte R, G, B;
 
-                    R = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
-                    counter++;
-                    G = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
-                    counter++;
-                    B = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
-                    counter++;
+                        R = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
+                        counter++;
+                        G = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
+                        counter++;
+                        B = Convert.ToByte(Math.Min(Math.Max(imageData[counter], 0), 1) * dataMultiplier);
+                        counter++;
 
-                    Color color = Color.FromArgb(255, R, G, B);
+                        Color color = Color.FromArgb(255, R, G, B);
 
-                    output.SetPixel(x, y, color);
+                        output.SetPixel(x, y, color);
+                    }
                 }
-            }
+            else
+                for (int x = 0; x < bitmapWidth; x++)
+                {
+                    for (int y = 0; y < bitmapHeight; y++)
+                    {
+                        byte greyscale = Convert.ToByte(Math.Max(0, Math.Min(1, imageData[counter])));
+                        Color color = Color.FromArgb(greyscale, greyscale, greyscale);
+                        output.SetPixel(x, y, color);
+
+                        counter++;
+                    }
+                }
             return output;
         }
 
-        public static List<double[]> GetImageVariations(Bitmap original)
+        public static List<double[]> GetImageVariations(Bitmap original, bool isGreyScale = false)
         {
             List<double[]> imagesData = new List<double[]>();
             imagesData.Add(BitmapToDoubleArray(original));
             Bitmap variation = new Bitmap(original);
             variation.RotateFlip(RotateFlipType.Rotate180FlipXY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
 
             variation = new Bitmap(original);
             variation.RotateFlip(RotateFlipType.Rotate90FlipNone);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate270FlipXY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate180FlipNone);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.RotateNoneFlipXY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate270FlipNone);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate90FlipXY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.RotateNoneFlipX);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate180FlipY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate90FlipX);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate270FlipY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate180FlipX);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.RotateNoneFlipY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate270FlipX);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
             variation = new Bitmap(original);
 
             variation.RotateFlip(RotateFlipType.Rotate90FlipY);
-            imagesData.Add(BitmapToDoubleArray(variation));
+            imagesData.Add(BitmapToDoubleArray(variation, isGreyScale));
             variation.Dispose();
 
             return imagesData;
