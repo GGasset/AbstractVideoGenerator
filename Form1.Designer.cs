e99b4f747698dba3a230f1ae1b17f4fa462ab654
Germán Gasset <gassetgerman@gmail.com>
2023-01-27 14:33:00 +0100

    added ShowGanImageBttn

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.Display = new System.Windows.Forms.PictureBox();
-            this.ShowAutoencoderImageBttn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ShowGanImageBttn = new System.Windows.Forms.Button();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.AutoencoderVideoSelectedImageBttn = new System.Windows.Forms.Button();
             this.LoadedNetworksLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Display)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,28 +51,17 @@
             this.Display.TabIndex = 2;
             this.Display.TabStop = false;
             // 
-            // ShowAutoencoderImageBttn
+            // ShowGanImageBttn
             // 
-            this.ShowAutoencoderImageBttn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.ShowGanImageBttn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ShowAutoencoderImageBttn.Location = new System.Drawing.Point(6, 19);
-            this.ShowAutoencoderImageBttn.Name = "ShowAutoencoderImageBttn";
-            this.ShowAutoencoderImageBttn.Size = new System.Drawing.Size(415, 23);
-            this.ShowAutoencoderImageBttn.TabIndex = 3;
-            this.ShowAutoencoderImageBttn.Text = "Show autoencoder image";
-            this.ShowAutoencoderImageBttn.UseVisualStyleBackColor = true;
-            this.ShowAutoencoderImageBttn.Click += new System.EventHandler(this.ShowAutoencoderImageBttn_Click);
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(6, 77);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(415, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Show autoencoder video from random image";
-            this.button2.UseVisualStyleBackColor = true;
+            this.ShowGanImageBttn.Location = new System.Drawing.Point(6, 19);
+            this.ShowGanImageBttn.Name = "ShowGanImageBttn";
+            this.ShowGanImageBttn.Size = new System.Drawing.Size(415, 23);
+            this.ShowGanImageBttn.TabIndex = 3;
+            this.ShowGanImageBttn.Text = "Show Gans image";
+            this.ShowGanImageBttn.UseVisualStyleBackColor = true;
+            this.ShowGanImageBttn.Click += new System.EventHandler(this.ShowGanImageBttn_Click);
             // 
             // flowLayoutPanel1
             // 
@@ -78,42 +69,65 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.flowLayoutPanel1.Controls.Add(this.groupBox2);
             this.flowLayoutPanel1.Controls.Add(this.groupBox1);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(159, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(437, 275);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(437, 150);
             this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(427, 78);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Autoencoder network";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(6, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(415, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Show autoencoder video from image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AutoencoderVideoSelectedImageBttn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(6, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(415, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Show autoencoder image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ShowAutoencoderImageBttn_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.groupBox1.Controls.Add(this.AutoencoderVideoSelectedImageBttn);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.ShowAutoencoderImageBttn);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Controls.Add(this.ShowGanImageBttn);
+            this.groupBox1.Location = new System.Drawing.Point(3, 87);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(427, 110);
+            this.groupBox1.Size = new System.Drawing.Size(427, 48);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Autoencoder network";
-            // 
-            // AutoencoderVideoSelectedImageBttn
-            // 
-            this.AutoencoderVideoSelectedImageBttn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.AutoencoderVideoSelectedImageBttn.Location = new System.Drawing.Point(6, 48);
-            this.AutoencoderVideoSelectedImageBttn.Name = "AutoencoderVideoSelectedImageBttn";
-            this.AutoencoderVideoSelectedImageBttn.Size = new System.Drawing.Size(415, 23);
-            this.AutoencoderVideoSelectedImageBttn.TabIndex = 6;
-            this.AutoencoderVideoSelectedImageBttn.Text = "Show autoencoder video from image";
-            this.AutoencoderVideoSelectedImageBttn.UseVisualStyleBackColor = true;
-            this.AutoencoderVideoSelectedImageBttn.Click += new System.EventHandler(this.AutoencoderVideoSelectedImageBttn_Click);
+            this.groupBox1.Text = "Gans";
             // 
             // LoadedNetworksLabel
             // 
             this.LoadedNetworksLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.LoadedNetworksLabel.AutoSize = true;
-            this.LoadedNetworksLabel.Location = new System.Drawing.Point(12, 294);
+            this.LoadedNetworksLabel.Location = new System.Drawing.Point(12, 169);
             this.LoadedNetworksLabel.Name = "LoadedNetworksLabel";
             this.LoadedNetworksLabel.Size = new System.Drawing.Size(95, 13);
             this.LoadedNetworksLabel.TabIndex = 7;
@@ -124,7 +138,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(598, 316);
+            this.ClientSize = new System.Drawing.Size(598, 191);
             this.Controls.Add(this.LoadedNetworksLabel);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.Display);
@@ -134,6 +148,7 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Display)).EndInit();
             this.flowLayoutPanel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -146,12 +161,13 @@
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadFromFileToolStripMenuItem;
         private System.Windows.Forms.PictureBox Display;
-        private System.Windows.Forms.Button ShowAutoencoderImageBttn;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ShowGanImageBttn;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button AutoencoderVideoSelectedImageBttn;
         private System.Windows.Forms.Label LoadedNetworksLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
